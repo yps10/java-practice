298cd093528a3ee3bf3f30f1954a8da6b005e48f
Yogendra Pratap Singh <yps10012000@gmail.com>
2023-05-24 18:22:37 +0530

    Selection sort using java language

--- src/Duplicate.java
+++ src/Duplicate.java
@@ -4,9 +4,9 @@
     public class Duplicate{
 
         public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);// for taking input
             System.out.print("Enter the size of the array: ");
-            int x = sc.nextInt();
+            int x = sc.nextInt();//value stored 
             int[] arr = new int[x];
 
             System.out.print("Enter " + x + " values: ");
