e99277e8139a8fc1696ac2138f45f0bdf1199679
Yogendra Pratap Singh <yps10012000@gmail.com>
2023-06-14 22:56:14 +0530

    second largest element in array

---
+++ src/Mm.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Mm {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        int n=5;
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+}
