075dade6ba71cb57aed9130fea3d20bbf9babb18
Yogendra Pratap Singh <yps10012000@gmail.com>
2023-07-13 23:43:23 +0530

    arry sort

---
+++ src/ArraySort.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ArraySort {
+    public static void main(String[] args) {
+
+
+        int[] arr = {2, 5, 6, -3, 1, 9};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
